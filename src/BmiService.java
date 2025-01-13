1bedd8fedb333dd823f1dd8ff2717d1f7af96613
Valeriya Mishurova <tvoyadenisova@yandex.ru>
2025-01-13 23:11:30 +0300

    new

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,8 @@
 public class BmiService {
-    public double calculate (double meters, int kilograms) {
+    public int calculate(double meters, int kilograms) {
         double index;
         double meters2 = meters * meters;
-        index = kilograms/ meters2;
+        index = kilograms / meters2;
         return (int) index;
     }
 }
